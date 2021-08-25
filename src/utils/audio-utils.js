d3a7bdbdb3c42b41d1fb32874e1ee050abec74bb
Dosaki <lord.g.bug@gmail.com>
2021-08-25 09:39:50 +0100

    Fixed a problem with Firefox.

--- src/utils/audio-utils.js
+++ src/utils/audio-utils.js
@@ -2,14 +2,16 @@ window.audioContext = null;
 let defaultDestination = null;
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 
+const isFirefox = typeof InstallTrigger !== 'undefined'; // This is because firefox has a bug with exponentialRampToValueAtTime
+
 export const play = (type, frequency, duration, trail, destination) => {
     if (!window.audioContext) {
         window.audioContext = new AudioContext();
         defaultDestination = window.audioContext.destination;
     }
     const _destination = destination || window.audioContext.destination;
-    const _duration = duration || null;
     const _trail = trail || 0.1;
+    const _duration = isFirefox ? _trail*100 : (duration || null);
     const _frequency = frequency || 440.0;
 
     const gain = window.audioContext.createGain();
@@ -24,6 +26,6 @@ export const play = (type, frequency, duration, trail, destination) => {
     if (_duration) {
         setTimeout(() => {
             oscilator.stop();
-        }, duration);
+        }, _duration);
     }
 };
